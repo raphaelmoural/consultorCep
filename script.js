26693ae0483d9deccbdc285a8550bd8d1cdd1df8
Raphael <raphael21moura@gmail.com>
2023-11-20 15:52:42 -0300

    20/11

--- script.js
+++ script.js
@@ -1,25 +1,87 @@
-document.getElementById('searchButton').addEventListener('click', function() {
-    const state = document.getElementById('state').value;
-    const city = document.getElementById('city').value;
-    const street = document.getElementById('street').value;
-    console.log(state, city, street);
-    fetchStreetName(state, city, street);
-})
+function toggleConsultaSection() { 
+    const select = document.getElementById('consultaSelect');
+    const cepSection = document.getElementById('cepSection');
+    const enderecoSection = document.getElementById('enderecoSection');
+    const resultadoElement = document.getElementById('resultado');
 
-async function fetchStreetName(state, city, street) {
+    if (select.value === 'cep') {
+        cepSection.style.display = 'block';
+        enderecoSection.style.display = 'none';
+    } else {
+        cepSection.style.display = 'none';
+        enderecoSection.style.display = 'block';
+    }
+    resultadoElement.innerHTML = '';
+}
+
+async function consultarEnderecoPorCep() {
+    const cep = document.getElementById('cepInput').value;
+    const resultadoElement = document.getElementById('resultado');
+
     try {
-        console.log("2", state, city, street)
-        const response = await axios.get(`https://viacep.com.br/ws/${state}/${city}/${street}/json/`);
-        console.log(response.data)
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+        const { logradouro, localidade, uf } = response.data;
 
-        const {logradouro, bairro, localidade} = response.data;
+        resultadoElement.innerHTML = `
+            <p><strong>Endereço encontrado:</strong></p>
+            <p>Rua: ${logradouro || 'Não informado'}</p>
+            <p>Cidade: ${localidade || 'Não informado'}</p>
+            <p>Estado: ${uf || 'Não informado'}</p>
+        `;
 
-        document.getElementById('streetName').innerHTML = logradouro || "Rua não encontrada.";
-        document.getElementById('neighName').innerHTML = bairro || "Bairro não encontrado.";
-        document.getElementById('locality').innerHTML = localidade || "Cidade não encontrada.";
+        cepInput.value = '';
 
-    } catch(error) {
-        console.log(error);
-        document.getElementById('streetName').innerHTML = 'Erro ao buscar endereço.'
+    } catch (error) {
+        console.error('Erro ao consultar CEP:', error);
+        resultadoElement.innerHTML = '<p><strong>Endereço não encontrado.</strong></p>';
     }
 }
+
+
+async function buscarCepPorEndereco() {
+    const rua = document.getElementById('ruaInput').value;
+    const cidade = document.getElementById('cidadeInput').value;
+    const estado = document.getElementById('estadoInput').value;
+    const resultadoElement = document.getElementById('resultado');
+
+    try {
+        const response = await axios.get(`https://viacep.com.br/ws/${estado}/${cidade}/${rua}/json/`);
+        
+        if (!response.data.erro) {
+            const endereco = {
+                rua: response.data.logradouro || 'Não informado',
+                cidade: response.data.localidade || 'Não informado',
+                estado: response.data.uf || 'Não informado',
+                cep: response.data.cep || 'Não informado'
+            };
+
+            resultadoElement.innerHTML = `
+                <p><strong>Endereço encontrado:</strong></p>
+                <p>Rua: ${endereco.rua}</p>
+                <p>Cidade: ${endereco.cidade}</p>
+                <p>Estado: ${endereco.estado}</p>
+                <p>CEP: ${endereco.cep}</p>
+            `;
+
+            ruaInput.value = '';
+            cidadeInput.value = '';
+            estadoInput.value = '';
+        } else {
+            resultadoElement.innerHTML = '<p><strong>CEP não encontrado para o endereço informado.</strong></p>';
+        }
+    } catch (error) {
+        console.error('Erro ao buscar CEP:', error);
+        resultadoElement.innerHTML = '<p><strong>Erro ao buscar CEP.</strong></p>';
+    }
+}
+
+
+const select = document.getElementById('consultaSelect');
+const consultarCepButton = document.getElementById('consultarCepButton');
+const consultarEnderecoButton = document.getElementById('consultarEnderecoButton');
+
+select.addEventListener('change', toggleConsultaSection);
+consultarCepButton.addEventListener('click', consultarEnderecoPorCep);
+consultarEnderecoButton.addEventListener('click', buscarCepPorEndereco);
+
+toggleConsultaSection();
